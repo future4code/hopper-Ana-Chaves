826fb87ddf60614e03c83f7034850a1db42ed2f8
Ana Chaves <aninhaj223@gmail.com>
2022-02-17 23:02:44 -0300

    Exercícios Funçôes

---
+++ modulo1/Funcoes/template_javascript/index.js
@@ -0,0 +1,23 @@
+// Exercícios de interpretação de código
+
+/*1.a) 10 e 50
+    b) A função será invocada e a operação irá acontecer mas não será exibido no console
+      pois foi retirado o console.log
+
+  2.a) A função formata o texto todo em minúsculo e verifica se tem o argumento citado no texto.
+    b) eu gosto de cenoura - true
+       cenoura é bom pra vista - true
+       cenouras crescem na terra - false
+
+*/   
+ // Exercícios de escrita de código
+ 
+  //1.
+
+  // Exercícios não finalizados- será finalizados e enviados para o git. 
+
+
+
+
+
+
