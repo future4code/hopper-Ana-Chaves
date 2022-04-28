2195061163d29a8d23b652d3d7142288dbecbeca
Ana Chaves <aninhaj223@gmail.com>
2022-04-28 19:15:18 -0300

    PR

---
+++ modulo3/props-labedin/src/components/CardPequeno/CardPequeno.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './CardPequeno.css'
+
+export const CardPequeno = () => {
+  return (
+    <div className='CardPequeno'>
+        <h1>Ana saddsaf sdfsdf DFDF SDFSDFSdf sdfsdfas SDF</h1>
+    </div>
+ 
+
+ )}
