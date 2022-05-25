f4889f9e1213792d6465557f159cf98e93f29105
Ana Chaves <aninhaj223@gmail.com>
2022-05-24 21:36:33 -0300

    Funcoes princinpais concluidas

--- modulo3/apis-labenusers/apis-labenusers/src/App.jsx
+++ modulo3/apis-labenusers/apis-labenusers/src/App.jsx
@@ -1,58 +1,24 @@
 import React from 'react'
 import  axios from 'axios'
+import Cadastro from './Component/Cadastro'
+import Listagem from './Component/Listagem'
 
 export default class App extends React.Component {
-  state ={
-    name:"" ,
-    email:"" ,
-  }
-  componentDidMount = () =>{
-    this.createusers()
-  }
+ state = {
+   handlePage:"cadastro",
+ }
 
- createusers = () =>{
-    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users' , {
-      headers:{
-        "Authorization": 'ana-chaves-hooper'
-      }
-    }).then((resposta) =>{
-      this.setState({name: resposta.data.result.list})
-    }).cath((error)=>{
-      alert(error.message)
-    })
-  }
-
-  handlename= (e) =>{
-    this.setState({name:e.target.value})
-  }
-  handleemail= (e) =>{
-    this.setState({email:e.target.value})
-  }
-
+ changePage = () =>{
+   this.setState({handlePage: this.state.handlePage === "cadastro" ? "listagem" : "cadastro"})
+ }
   
   render() {
-    // const rendernomes = this.state.name.map((nomes)=>{
-    //   return <p>{nomes.name}</p>
-    // })
     return (
       <div>
-        <div>
-          <input type="text" placeholder="Nome" 
-            value={this.state.name} 
-            onChange={this.handlename}
-          />
-        </div>
-          <input type="text" placeholder="E-mail" 
-          value={this.state.email}
-          onChange={this.handleemail}
-          />
-        <button>Criar Usuario</button>
-       <div>
-        <button>trocar de teala</button>
-        </div>  
-        <div>
-          {/* <p>{rendernomes}</p> */}
-        </div>
+        <button onClick={this.changePage}>Trocar de Tela</button>
+        {
+          this.state.handlePage ==="cadastro" ? <Cadastro /> : <Listagem />
+        }
       </div>
     )
   }
