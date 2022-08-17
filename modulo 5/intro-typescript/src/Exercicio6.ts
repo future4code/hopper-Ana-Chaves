c3afa5e7a7525ec55d2b87a0e1d06aeaec165872
Ana Chaves <anajuchaves03@gmail.com>
2022-08-17 20:35:56 -0300

    Exercicios

---
+++ modulo 5/intro-typescript/src/Exercicio6.ts
@@ -0,0 +1,17 @@
+function operacoes (n1:number, n2:number, callback:number ):void{
+    let som= n1 +n2
+    let sub= n1 - n2
+    let mult= n1 * n2
+  
+    
+}
+
+function maiornumero(n1:number, n2:number, ):number{
+    if (n1 > n2) {
+        return (n1)
+    } else {
+        return (n2)
+    }
+}
+
+console.log(operacoes)
